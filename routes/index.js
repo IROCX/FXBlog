78f042e4e4043fccf3c106d00dd3b98f4cd98395
IROCX <irocx.7@gmail.com>
2023-12-16 17:47:39 +0530

    Refactor index.js in routes

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,6 @@
-var express = require('express')
-var router = express.Router()
+const commentRoutes = require('./comments');
+const campgroundRoutes = require('./campgrounds');
+const homeRoutes = require('./home');
+const userRoutes = require('./users');
 
-var passport = require('passport')
-var User = require('../models/user')
-
-
-router.get('/', (req, res) => {
-    res.redirect('/campgrounds')
-})
-
-router.get('/about', (req, res) => {
-    res.render('about')
-})
-
-//==================AUTH ROUTES===============================
-
-router.get('/login', (req, res) => {
-    res.render('forms/login')
-})
-
-
-router.post('/login', passport.authenticate('local',
-    {
-        successRedirect: '/campgrounds',
-        failureRedirect: '/login'
-    }), (req, res) => {
-    })
-
-router.get('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/login')
-})
-
-
-router.get('/register', (req, res) => {
-    res.render('forms/register')
-})
-router.post('/register', (req, res) => {
-    var newUser = new User({ username: req.body.username, name: req.body.name, contact: req.body.contact, email: req.body.email })
-    User.register(newUser, req.body.password, (error, ir) => {
-        if (error) {
-            console.log('Error')
-            return res.render('register')
-        }
-        passport.authenticate('local')(req, res, () => {
-            res.redirect('/campgrounds')
-        })
-    })
-})
-
-module.exports = router
+module.exports = {commentRoutes, campgroundRoutes, homeRoutes, userRoutes};
